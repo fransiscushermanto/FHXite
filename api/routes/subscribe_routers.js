cca46f211357903686c9f6981439516e96f2e9ea
Fransiscus Hermanto <fransiscus.huang47@gmail.com>
2019-12-25 23:38:15 +0700

    change subscribe_routers.js

--- api/routes/subscribe_routers.js
+++ api/routes/subscribe_routers.js
@@ -10,8 +10,11 @@ router.get("/get", async (req, res) => {
 router.post("/subs", async (req, res) => {
   const sub = await Subs.create({
     email: req.body.username
-  }).catch(err => console.error(err));
-  res.json(sub);
+  }).catch(err => {
+    console.error(err);
+    res.status(422).json(error);
+  });
+  res.json(sub); //
 });
 
 router.post("/delete", async (req, res) => {
